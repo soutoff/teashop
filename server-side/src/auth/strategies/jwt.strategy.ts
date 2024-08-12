51caef14727409c7d2d4199c1f2b41ede4067399
soutoff <i@oforever.ru>
2024-08-12 09:57:44 +0300

    создал стратегии jwt, yandex, google, получил client id

---
+++ server-side/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UserService } from "src/user/user.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor (private configService: ConfigService, private userServcice: UserService) {
+    super({
+      jwtFeromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: configService.get('JWT_SECTRET')
+    })
+  }
+
+  async validate({id}: {id: string}) {
+    return this.userServcice.getById(id)
+  }
+}
