80e34915ae61ff1490f1c1486c756a697b0556f7
soutoff <i@oforever.ru>
2024-08-12 11:33:49 +0300

    Методы добавления и удаления токенов, Валидация для Yandex Google

--- server-side/src/auth/auth.service.ts
+++ server-side/src/auth/auth.service.ts
@@ -3,10 +3,15 @@ import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
 import { UserService } from 'src/user/user.service';
 import { AuthDto } from './dto/auth.dto';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor (private jwt: JwtService, private userService: UserService, private prisma: PrismaService) {}
+  EXPIRE_DAY_REFRESH_TOKEN = 1
+  REFRESH_TOKEN_NAME = "refreshToken"
+
+  constructor (private jwt: JwtService, private userService: UserService, private prisma: PrismaService, private configService: ConfigService) {}
 
   async login(dto: AuthDto) {
     const user = await this.validateUser(dto)
@@ -33,7 +38,7 @@ export class AuthService {
 
     if (!result) throw new UnauthorizedException('Невалидный refres токен')
 
-    const user = await this.validateUser(dto)
+    const user = await this.userService.getById(result.id)
     const tokens = this.issueTokens(user.id)
   
     return {user, ...tokens}
@@ -62,6 +67,54 @@ export class AuthService {
     if(!user) throw new NotFoundException('Пользователь не найден')
 
     return user  
+  }
+
+  // Валтдфция для Yandex Google
+  async validateOAuthLogin(req: any) {
+    let user = await this.userService.getByEmail(req.user.email)
+
+    if (!user) {
+      user = await this.prisma.user.create({
+        data: {
+          email: req.user.email,
+          name: req.user.name,
+          picture: req.user.picture,
+        },
+        include: {
+          stores: true,
+          favorites: true,
+          orders: true
+        }
+      })
+
+      const tokens = this.issueTokens(user.id)
+
+      return {user, ...tokens}
+    }
+  }
+
+  // Методы добавления и удаления токенов
+  addRefreshTokenToResponse (res: Response, refreshToken: string) {
+    const expiresIn = new Date()
+    expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+
+    res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+      httpOnly: true,
+      domain: this.configService.get('SERVER_DOMAIN'),
+      expires: expiresIn,
+      secure:true,
+      sameSite:'none'
+    })
+  }
+
+  removeRefreshTokenFromResponse (res: Response) {
+    res.cookie(this.REFRESH_TOKEN_NAME, '', {
+      httpOnly: true,
+      domain: this.configService.get('SERVER_DOMAIN'),
+      expires: new Date(0),
+      secure:true,
+      sameSite:'none'
+    })
   }
 
 }
